bbcad9d3479e4a6a0feeec894059703d3c6a21ba
qikke <qikke1995@126.com>
2018-08-30 20:34:39 +0800

    add button group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './components/button'
 import Svg from './components/svg'
+import ButtonGroup from './components/button-group'
 
 Vue.component('g-button',Button)
 Vue.component('g-svg',Svg)
+Vue.component('g-button-group',ButtonGroup)
 
 new Vue({
   el:'#app',
