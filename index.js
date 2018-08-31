10f05d79ac4b0b08ad167ad4998d0e90f4a0c368
qikke <qikke1995@126.com>
2018-08-31 12:23:19 +0800

    创建index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import Button from './src/components/button'
+import ButtonGroup from './src/components/button-group'
+import Svg from './src/components/svg'
+
+
+export {
+  Button,
+  ButtonGroup,
+  Svg
+}
