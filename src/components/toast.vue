18f240d11f792508bea8803990c9a5d945751989
qikke <qikke1995@126.com>
2018-09-06 11:20:12 +0800

    toast.vue auto close

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -6,7 +6,24 @@
 
 <script>
   export default {
-    name: "toast"
+    name: "k-toast",
+    props: {
+      autoClose: {type: Boolean, default: true},
+      autoCloseDelay: {type: Number, default: 3}
+    },
+    mounted(){
+      if(this.autoClose){
+        window.setTimeout(()=>{
+          this.close()
+        },this.autoCloseDelay*1000)
+      }
+    },
+    methods:{
+      close(){
+        this.$el.remove()
+        this.$destroy()
+      }
+    }
   }
 </script>
 
@@ -14,20 +31,7 @@
   $font-size: 14px;
   $toast-height: 40px;
   $toast-bg: rgba(0, 0, 0, 0.75);
-  .toast {
-    font-size: $font-size;
-    height: $toast-height;
-    line-height: 1.8;
-    position: fixed;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    display:flex;
-    color:white;
-    align-items: center;
-    background: $toast-bg;
-    border-radius: 4px;
-    box-shadow: 0 0 3px 0 rgba(0,0,0,0.5);
-    padding:0 16px;
+  .toast {font-size: $font-size;height: $toast-height;line-height: 1.8;position: fixed;top: 0;left: 50%;transform: translateX(-50%);display: flex;
+    color: white;align-items: center;background: $toast-bg;border-radius: 4px;box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);padding: 0 16px;
   }
 </style>
