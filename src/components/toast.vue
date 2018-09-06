3322a8b5f72f14ed72dcea4886514c5e871ad7b0
qikke <qikke1995@126.com>
2018-09-06 11:38:04 +0800

    add closeButton

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -2,7 +2,7 @@
   <div class="toast">
     <slot></slot>
     <div class="line"></div>
-    <span></span>
+    <span class="close" @click="closeButton.callback()">{{closeButton.text}}</span>
   </div>
 </template>
 
@@ -11,17 +11,28 @@
     name: "k-toast",
     props: {
       autoClose: {type: Boolean, default: true},
-      autoCloseDelay: {type: Number, default: 3}
-    },
-    mounted(){
-      if(this.autoClose){
-        window.setTimeout(()=>{
-          this.close()
-        },this.autoCloseDelay*1000)
+      autoCloseDelay: {type: Number, default: 3},
+      closeButton: {
+        type: Object,
+        default: () => {
+          return {
+            text: 'close',
+            callback: () => {
+              console.log('close的默认回调')
+            }
+          }
+        }
       }
     },
-    methods:{
-      close(){
+    mounted() {
+      if (this.autoClose) {
+        window.setTimeout(() => {
+          this.close()
+        }, this.autoCloseDelay * 1000)
+      }
+    },
+    methods: {
+      close() {
         this.$el.remove()
         this.$destroy()
       }
@@ -35,12 +46,12 @@
   $toast-bg: rgba(0, 0, 0, 0.75);
   .toast {font-size: $font-size;height: $toast-height;line-height: 1.8;position: fixed;top: 0;left: 50%;transform: translateX(-50%);display: flex;
     color: white;align-items: center;background: $toast-bg;border-radius: 4px;box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);padding: 0 16px;
-    .line{
-      height:100%;
-      border-left:1px solid #666;
+    .line {
+      height: 100%;
+      border-left: 1px solid #666;
       margin-left: 16px;
     }
-    .close{
+    .close {
       padding-left: 16px;
     }
   }
