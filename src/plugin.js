9e1906151c0ff2c21418355281655031c60e53ab
qikke <qikke1995@126.com>
2018-09-06 11:39:40 +0800

    add toast's propsData

--- src/plugin.js
+++ src/plugin.js
@@ -1,9 +1,19 @@
 import Toast from './components/toast'
+
 export default {
-  install(Vue,options){
-    Vue.prototype.$toast = function(message){
+  install(Vue, options) {
+    Vue.prototype.$toast = function (message) {
       let Constructor = Vue.extend(Toast)
-      let toast = new Constructor
+      let toast = new Constructor({
+        propsData: {
+          closeButton: {
+            text: 'xxxx',
+            callback: () => {
+              console.log('this is callback !')
+            }
+          }
+        }
+      })
       toast.$slots.default = message
       toast.$mount()
       document.body.appendChild(toast.$el)
