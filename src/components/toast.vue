03abd02efd95396293f27258f21efbc02f2bffe4
qikke <qikke1995@126.com>
2018-09-06 11:30:23 +0800

    补充 close,line css

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="toast">
     <slot></slot>
+    <div class="line"></div>
+    <span></span>
   </div>
 </template>
 
@@ -33,5 +35,13 @@
   $toast-bg: rgba(0, 0, 0, 0.75);
   .toast {font-size: $font-size;height: $toast-height;line-height: 1.8;position: fixed;top: 0;left: 50%;transform: translateX(-50%);display: flex;
     color: white;align-items: center;background: $toast-bg;border-radius: 4px;box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);padding: 0 16px;
+    .line{
+      height:100%;
+      border-left:1px solid #666;
+      margin-left: 16px;
+    }
+    .close{
+      padding-left: 16px;
+    }
   }
 </style>
