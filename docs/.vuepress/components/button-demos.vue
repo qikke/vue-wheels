27a4bf59bba3d9246b2ee4f8eb7caff853ab73e4
qikke <qikke1995@126.com>
2018-09-11 19:58:25 +0800

    修改button样式

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -2,30 +2,30 @@
   <div>
     <ol class="description">
       <li>
-        <KButton>无icon</KButton>
+        <k-button>无icon</k-button>
         <pre><code>{{content.noIcon}}</code></pre>
       </li>
       <li>
-        <KButton icon="setting">leftIcon</KButton>
+        <k-button icon="setting">leftIcon</k-button>
         <pre><code>{{content.leftIcon}}</code></pre>
       </li>
       <li>
-        <KButton icon="setting" iconPosition="right">rightIcon</KButton>
+        <k-button icon="setting" iconPosition="right">rightIcon</k-button>
         <pre><code>{{content.rightIcon}}</code></pre>
       </li>
       <li>
-        <KButton :loading="true" icon="setting">loading</KButton>
+        <k-button :loading="true" icon="setting">loading</k-button>
         <pre><code>{{content.loading}}</code></pre>
       </li>
       <li>
-        <KButton disabled>disabled</KButton>
+        <k-button disabled>disabled</k-button>
         <pre><code>{{content.disabled}}</code></pre>
       </li>
       <li>
-        <KButton-group>
-          <KButton>按钮1</KButton>
-          <KButton>按钮2</KButton>
-        </KButton-group>
+        <k-button-group>
+          <k-button>按钮1</k-button>
+          <k-button>按钮2</k-button>
+        </k-button-group>
         <pre><code>{{content.buttonGroup}}</code></pre>
       </li>
     </ol>
@@ -44,16 +44,16 @@
     data(){
       return {
         content:{
-          noIcon:'<KButton>无icon</KButton>',
-          leftIcon:'<KButton icon="setting">leftIcon</KButton>',
-          rightIcon:'<KButton icon="setting" iconPosition="right">rightIcon</KButton>',
-          loading:'<KButton :loading="true" icon="setting">loading</KButton>',
-          disabled:'<KButton disabled>disabled</KButton>',
+          noIcon:'<k-button>无icon</k-button>',
+          leftIcon:'<k-button icon="setting">leftIcon</k-button>',
+          rightIcon:'<k-button icon="setting" iconPosition="right">rightIcon</k-button>',
+          loading:'<k-button :loading="true" icon="setting">loading</k-button>',
+          disabled:'<k-button disabled>disabled</k-button>',
           buttonGroup:`
-            <KButton-group>
-            <KButton>按钮1</KButton>
-            <KButton>按钮2</KButton>
-            </KButton-group>
+            <k-button-group>
+            <k-button>按钮1</k-button>
+            <k-button>按钮2</k-button>
+            </k-button-group>
           `.replace(/ /g,'').trim()
         }
       }
