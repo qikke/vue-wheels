3469ce86049943030a6c9944213c4942dfc38b8a
qikke <qikke1995@126.com>
2018-09-25 13:35:15 +0800

    测试更多数据，完善安装和启动页面

--- docs/get-started/README.md
+++ docs/get-started/README.md
@@ -3,3 +3,4 @@ title: 快速开始
 ---
 
 # get-started
+## 该页面正在修改
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 import Button from './src/components/button'
 import ButtonGroup from './src/components/button-group'
-import Svg from './src/components/icon'
+import Icon from './src/components/icon'
+import Col from './src/components/col'
+import Input from './src/components/input'
+import Popover from './src/components/popover'
+import Row from './src/components/row'
+import Toast from './src/components/toast'
+import Content from './src/layout/content'
+import Footer from './src/layout/footer'
+import Header from './src/layout/header'
+import Layout from './src/layout/layout'
+import Slider from './src/layout/slider'
+import Tabs from './src/tabs/tabs'
+import TabsPane from './src/tabs/tabs-pane'
+import TabsItem from './src/tabs/tabs-item'
+import TabsHeads from './src/tabs/tabs-heads'
+import TabsBody from './src/tabs/tabs-body'
+
 
 
 export {
   Button,
   ButtonGroup,
-  Svg
+  Icon,
+  Col,
+  Input,
+  Popover,
+  Row,
+  Toast,
+  Content,
+  Footer,
+  Header,
+  Layout,
+  Slider,
+  Tabs,
+  TabsPane,
+  TabsItem,
+  TabsHeads,
+  TabsBody,
 }
